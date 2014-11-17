5aaf44c951c1b202eef512e561baf728e26758c2
neket521 <nikita.kirienko@tkvg.ee>
2014-11-17 13:17:01 +0200

    step2_hash

---
+++ step2_hash.py
@@ -0,0 +1,9 @@
+def step2_hash(values_true, p_eriala_choose,k_eriala_hash):
+    values_true_hash = set()
+    for i in values_true:
+        if values_true[i] = 1:
+            values_true_hash.add(p_eriala_choose[i])
+    k_eriala_hash - values_true_hash
+
+    return k_eriala_hash
+
